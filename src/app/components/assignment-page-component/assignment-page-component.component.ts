edbef42c571fad5d4a8d763f87436f9e08d2327e
kalush666 <jonathan.kalush@gmail.com>
2025-12-30 20:13:48 +0200

    Refactor: Introduce stage-based UI in AssignmentPageComponent

--- src/app/components/assignment-page-component/assignment-page-component.component.ts
+++ src/app/components/assignment-page-component/assignment-page-component.component.ts
@@ -1,8 +1,6 @@
-import { Component, QueryList, ViewChildren } from '@angular/core';
-import { ClientConstants } from '../../common';
-import { MissionComponentComponent } from '../mission-component/mission-component.component';
-
-const { ADD_MISSION_LABEL, SUBMIT_LABEL } = ClientConstants.AssignmentPageConstants;
+import { Component, signal } from '@angular/core';
+import { Mission } from '../../models/mission.model';
+import { AssignmentStage } from '../../common/enums/assignmentStage.enum';
 
 @Component({
   selector: 'app-assignment-page-component',
@@ -11,37 +9,16 @@ const { ADD_MISSION_LABEL, SUBMIT_LABEL } = ClientConstants.AssignmentPageConsta
   styleUrl: './assignment-page-component.component.scss',
 })
 export class AssignmentPageComponentComponent {
-  @ViewChildren(MissionComponentComponent)
-  public missionComponents!: QueryList<MissionComponentComponent>;
+  public readonly AssignmentStage = AssignmentStage;
+  public currentStage = signal<AssignmentStage>(AssignmentStage.MANAGEMENT);
+  public missions = signal<Mission[]>([]);
 
-  public readonly addMissionLabel = ADD_MISSION_LABEL;
-  public readonly submitLabel = SUBMIT_LABEL;
-  public missionIds: number[] = [];
-
-  private nextMissionId = 0;
-
-  public onAddMission(): void {
-    this.missionIds.push(this.nextMissionId++);
+  public onMissionsSubmit(missions: Mission[]): void {
+    this.missions.set(missions);
+    this.currentStage.set(AssignmentStage.REVIEW);
   }
 
-  public onRemoveMission(missionId: number): void {
-    this.missionIds = this.missionIds.filter((id) => id !== missionId);
-  }
-
-  public onSubmit(): void {
-    const missions = this.missionComponents.map((component) => component.missionForm.value);
-    console.log('Submit missions:', missions);
-  }
-
-  public get isSubmitDisabled(): boolean {
-    if (this.missionIds.length === 0) {
-      return true;
-    }
-
-    return this.missionComponents?.some((component) => component.missionForm.invalid) ?? true;
-  }
-
-  public trackByMissionId(_index: number, missionId: number): number {
-    return missionId;
+  public onBackToManagement(): void {
+    this.currentStage.set(AssignmentStage.MANAGEMENT);
   }
 }
