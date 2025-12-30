fd80b81bf8b635e3eee7a6a78f135098a840ba11
kalush666 <jonathan.kalush@gmail.com>
2025-12-30 18:42:48 +0200

    [feat] basic validation

--- src/app/components/mission-component/mission-component.component.ts
+++ src/app/components/mission-component/mission-component.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UAVType } from '../../enums/uavType.enum';
 import { Priority } from '../../enums/priority.enum';
 
@@ -13,28 +13,36 @@ export class MissionComponentComponent {
   public readonly uavTypes = Object.values(UAVType);
   public readonly priority = Object.values(Priority);
 
+  public readonly remove = output<void>();
+
   public missionForm = new FormGroup({
-    missionId: new FormControl(''),
-    requiredUAVType: new FormControl(''),
-    priority: new FormControl(''),
+    missionId: new FormControl('', [Validators.required]),
+    requiredUAVType: new FormControl('', [Validators.required]),
+    priority: new FormControl('', [Validators.required]),
     location: new FormGroup({
-      latitude: new FormControl(0),
-      longitude: new FormControl(0),
-      altitude: new FormControl(0),
+      latitude: new FormControl(0, [Validators.required, Validators.min(-90), Validators.max(90)]),
+      longitude: new FormControl(0, [
+        Validators.required,
+        Validators.min(-180),
+        Validators.max(180),
+      ]),
+      altitude: new FormControl(0, [Validators.required]),
     }),
     timeWindow: new FormGroup({
       startTime: new FormGroup({
-        hours: new FormControl(0),
-        minutes: new FormControl(0),
-        seconds: new FormControl(0),
+        hours: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(23)]),
+        minutes: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(59)]),
+        seconds: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(59)]),
       }),
       endTime: new FormGroup({
-        hours: new FormControl(0),
-        minutes: new FormControl(0),
-        seconds: new FormControl(0),
+        hours: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(23)]),
+        minutes: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(59)]),
+        seconds: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(59)]),
       }),
     }),
   });
 
-  public onRemove(): void {}
+  public onRemove(): void {
+    this.remove.emit();
+  }
 }
